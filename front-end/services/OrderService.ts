83228daf0cfc80f0b46cb5d98700b6e4592dff3c
JashanpreetSinghUCLL <jashanpreet.singh@student.ucll.be>
2024-12-16 23:36:44 +0100

    Added getOrderById and minor changes in getAllOrders in orderService

--- front-end/services/OrderService.ts
+++ front-end/services/OrderService.ts
@@ -1,16 +1,42 @@
-const getAllOrders = () => {
+import { Order } from '@types';
+
+const getAllOrders = async (): Promise<Order[]> => {
     const token = JSON.parse(sessionStorage.getItem('loggedInUser')!).token;
-    return fetch(process.env.NEXT_PUBLIC_API_URL + '/orders', {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/orders', {
         method: 'GET',
         headers: {
             'content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
         },
     });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return await response.json();
+};
+
+const getOrderById = async (id: number): Promise<Order> => {
+    const token = JSON.parse(sessionStorage.getItem('loggedInUser')!).token;
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + `/orders/${id}`, {
+        method: 'GET',
+        headers: {
+            'content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return await response.json();
 };
 
 const OrderService = {
     getAllOrders,
+    getOrderById,
 };
 
 export default OrderService;
