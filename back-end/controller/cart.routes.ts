cde82f18ab2a4e5c330fee310365b172fca6ba10
BrentGrade <brent.grade@student.ucll.be>
2024-12-20 10:34:11 +0100

    BE swagger fix

--- back-end/controller/cart.routes.ts
+++ back-end/controller/cart.routes.ts
@@ -1,6 +1,11 @@
 /**
  * @swagger
  * components:
+ *    securitySchemes:
+ *     bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
  *   schemas:
  *     CartItem:
  *       type: object
